41f81236dadc69ed4ae5b4104b9940ea53888b44
Mahakalpa <mahakalpa.nagesh@amadeus.com>
2024-12-05 01:59:28 -0500

    Mahakalpa_code_changes

--- service/routes.py
+++ service/routes.py
@@ -99,19 +99,33 @@ def create_products():
 # L I S T   A L L   P R O D U C T S
 ######################################################################
 
-@app.route('/products/names', methods=['GET'])
-def list_products_by_name():
-    """
-    Lists all Products by Name
-    This endpoint will return a list of all Product names
-    """
-    app.logger.info("Request to list all Product names")
-
-    products = Product.query.all()
-    product_names = [product.name for product in products]
-
-    app.logger.info("Returning %d product names", len(product_names))
-    return jsonify(product_names), status.HTTP_200_OK
+@app.route("/products", methods=["GET"])
+def list_products():
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        # create enum from string
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
+    results = [product.serialize() for product in products]
+    app.logger.info("[%s] Products returned", len(results))
+    return results, status.HTTP_200_OK
 
 ######################################################################
 # R E A D   A   P R O D U C T
